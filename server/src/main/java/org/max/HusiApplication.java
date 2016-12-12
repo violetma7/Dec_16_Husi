f1ff200f649c9c92e20647b65862d10f9a0cf270
Nie Tao <nietao2@sina.com>
2016-12-12 22:18:13 +0800

    init

---
+++ server/src/main/java/org/max/HusiApplication.java
@@ -0,0 +1,12 @@
+package org.max;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HusiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HusiApplication.class, args);
+	}
+}
